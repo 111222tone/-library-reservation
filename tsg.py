32586bf26152c2f1df0992dd28df54a552bd986c
111222tone <monster0357@126.com>
2024-10-05 14:09:55 +0800

    Update tsg.py

--- tsg.py
+++ tsg.py
@@ -33,7 +33,7 @@ def job():
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39',
 }
     json_data1 = {
-    'cardNumber': '学号',
+    'cardNumber': '2120400212',
     'applyDate': t,
     'applyStartTime': '06:30',
     'applyDuration': '15.5',
